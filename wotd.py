94e0d75e57c8a4b603149fd2aa2d49e2c6de2d50
gabriel <dvabnnzsngf>
2026-01-29 23:09:44 -0600

    Initial

--- wotd.py
+++ wotd.py
@@ -1,7 +1,7 @@
 import re
 import requests
 
-WORD = 'Grandee'
+WORD = 'Pedant'
 REF_DICTIONARY = "collegiate"
 REF_THESAURUS = "thesaurus"
 DICTIONARY_KEY = 'f45f1248-4774-4d20-8d31-ecb2d70452e0'
@@ -58,7 +58,7 @@ def cleaner(clean_text, sharp=None):
     clean_text = re.sub(r"dst1", '', clean_text)
     clean_text = re.sub(r"]", '', clean_text)
     clean_text = re.sub(r"ds1a", '', clean_text)
-    clean_text = re.sub(r"ds1", '', clean_text)
+    clean_text = re.sub(r"ds2", '', clean_text)
     clean_text = re.sub(r"issue'", '', clean_text)
     # clean_text = re.sub(r"", '', clean_text)
     print(clean_text)
