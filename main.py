5aa9894cb1d5a965d546916b9dc67181f92577f6
gabriel <dvabnnzsngf>
2026-04-04 15:40:57 -0500

    Initial

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def sidebar(chosen_word, variant):
 
     if check_for_no_data(variant[favored].etymology):
         if st.sidebar.button("Etymology"):
-            for t in range(variant):
+            for t in range(len(variant)):
                 st.sidebar.markdown(variant[favored].etymology)
     else:
         pass
