be261496c94932792aec62c8ec628693441e42c8
gabriel <dvabnnzsngf>
2026-04-07 00:34:46 -0500

    Initial

--- wotd.py
+++ wotd.py
@@ -5,7 +5,6 @@ from cleaner_file import cleaner
 from cleaner_file import list_of_prev_wotd_cleaner
 
 WORD = 'winnow'
-chosen_word = 'aver'
 REF_DICTIONARY = "collegiate"
 REF_THESAURUS = "thesaurus"
 DICTIONARY_KEY = 'f45f1248-4774-4d20-8d31-ecb2d70452e0'
