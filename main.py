aa391b3e951a425d34e260dda30f5101f921ed86
gabriel <dvabnnzsngf>
2026-04-16 01:21:25 -0500

    Initial

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ def sidebar(chosen_word, variant):
     if st.sidebar.button('Previous words of the day.'):
         for t in previous_WOTD:
             if create_merriam_button(t, t):
-               display_photo(t)
+                    display_photo(t)
 
 
 
